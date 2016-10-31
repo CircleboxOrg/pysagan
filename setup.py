a2caf9d84afb177fdf6e9b9c2ab728c1936ebcae
Timothy Smith <tsmith@saberastro.com>
2016-10-31 14:57:11 +1100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='sagan',
+    version='',
+    packages=['sagan'],
+    requires=['smbus-cffi'],
+    url='',
+    license='',
+    author='T A H Smith',
+    author_email='tim@cuberider.com',
+    description='Python library for interfacing with sagan board'
+)
