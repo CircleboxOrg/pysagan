2ed17a3fb7bbeb297d691e1d8b961134b90c086f
tahsmith <tim@cuberider.com>
2016-11-01 14:49:56 +1100

    dump barometer readings

--- sagan/barometer.py
+++ sagan/barometer.py
@@ -5,9 +5,9 @@ class Barometer(I2cDevice):
     """
     Interface for BME280 pressure and humidity
     """
-
     def measure(self):
-        pass
+        frame = self.read(0xF7, 0xFE - 0xF7)
+        return frame
 
     def test(self):
         id = self.read(0xD0, 1)[0]
