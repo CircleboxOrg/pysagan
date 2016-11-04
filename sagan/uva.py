93dd13235d65aee74819640d4638bda48404c225
Tim Smith <tim@cuberider.com>
2016-11-04 04:30:56 +0000

    Use lower lever bus functions for UVA

--- sagan/uva.py
+++ sagan/uva.py
@@ -7,10 +7,10 @@ class UvaSensor(I2cDevice):
         return True
 
     def configure(self, args: dict) -> None:
-        self.write(0x38, [0x60])
+        self.bus.write_byte(0x38, 0x60)
 
     def measure(self):
         # scale from sensor is 5 uW / cm^2  / encoder count.
-        msb = self.read(0x38, 1)[0]
-        lsb = self.read(0x39, 1)[0]
+        msb = self.bus.read_byte_data(0x39, 0x00)
+        lsb = self.bus.read_byte_data(0x38, 0x00)
         return ((msb << 8) | lsb) * 5e-6
