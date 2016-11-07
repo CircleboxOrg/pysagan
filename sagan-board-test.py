fdddc45fd87b6b454c8df2629b188cbfe37f28f9
Tim Smith <tim@cuberider.com>
2016-11-07 01:46:15 +0000

    add sagan-board-test.py for checking board

---
+++ sagan-board-test.py
@@ -0,0 +1,43 @@
+from smbus import SMBus
+from sagan.barometer import Barometer
+from sagan.temperature import TemperatureSensor
+from sagan.imu import AccelerometerMagnetometer, Gyroscope
+from sagan.rgb_ir import RgbIrSensor
+from sagan.uva import UvaSensor
+from sagan.rtc import Rtc
+from sagan.leds import Leds
+from time import sleep
+
+bus = SMBus(1)
+barometer = Barometer(bus, 0x76)
+bottom_temperature = TemperatureSensor(bus, 0x48)
+top_temperature = TemperatureSensor(bus, 0x49)
+acc_mag = AccelerometerMagnetometer(bus, 0x1D)
+gyro = Gyroscope(bus, 0x6b)
+rgb_ir = RgbIrSensor(bus, 0x52)
+uva = UvaSensor(bus, 0x38)
+rtc = Rtc(bus, 0x51)
+
+sensors = [barometer, bottom_temperature, top_temperature, acc_mag, gyro, rgb_ir, uva, rtc]
+
+print(all(sensor.self_test() for sensor in sensors))
+
+for sensor in sensors:
+    sensor.configure({})
+
+for sensor in sensors:
+    print(sensor.measure())
+
+leds = Leds()
+
+leds.set_led1('on')
+leds.set_led2('on')
+leds.set_red('on')
+leds.set_green('on')
+leds.set_blue('on')
+sleep(1)
+leds.set_led1('off')
+leds.set_led2('off')
+leds.set_red('off')
+leds.set_green('off')
+leds.set_blue('off')
