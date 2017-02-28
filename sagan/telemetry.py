dd27f676d196f78c00203f1ff1bdbbf9dba0c4c6
AlexCollins <Alexander@MacBook-Pro.local>
2017-02-28 18:16:51 +1100

    adding Telemetry new lines for FIFO

--- sagan/telemetry.py
+++ sagan/telemetry.py
@@ -6,7 +6,7 @@ class Telemetry:
     @staticmethod
     def update(prefix: str, data: str):
         try:
-            _FIFO.write("{}:{}".format(prefix, data))
+            _FIFO.write("{}:{}\n".format(prefix, data))
         except:
             return False
         return True
