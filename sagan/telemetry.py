0c20ab9e52df0b40ad00841b684a60e54a66d206
AlexCollins <Alexander@MacBook-Pro.local>
2017-03-01 13:19:31 +1100

    cancel double quotations

--- sagan/telemetry.py
+++ sagan/telemetry.py
@@ -6,7 +6,7 @@ class Telemetry:
     @staticmethod
     def update(prefix: str, data: str):
         try:
-            _FIFO.write("{}:{}\n".format(prefix, data))
+            _FIFO.write("{}:{}\n".format(prefix, data.replace('"', '\\"'))
             _FIFO.flush()
         except:
             return False
