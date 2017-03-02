d99a6596754d05d5eb0c7ba553e77f6796fc5fa9
AlexCollins <Alexander@MacBook-Pro.local>
2017-03-02 12:29:42 +1100

    more syntax

--- sagan/imu.py
+++ sagan/imu.py
@@ -79,7 +79,7 @@ class Accelerometer(Lsm9ds0I2cDevice):
             "z": str(result[2])
         }
 
-        Telemetry.update("acc", "{}".format(packet)
+        Telemetry.update("acc", "{}".format(packet))
         return result
 
     @property
@@ -127,7 +127,7 @@ class Magnetometer(Lsm9ds0I2cDevice):
             "z": str(result[2])
         }
 
-        Telemetry.update("mag", "{}".format(packet)
+        Telemetry.update("mag", "{}".format(packet))
 
         return result
 
