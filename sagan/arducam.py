b54e2c41bca921022a9e65316f00cb9394231c9f
AlexCollins <Alexander@MacBook-Pro.local>
2017-03-02 14:50:32 +1100

    fixing magic

--- sagan/arducam.py
+++ sagan/arducam.py
@@ -3,7 +3,6 @@ from .telemetry import Telemetry
 from collections import namedtuple
 from datetime import datetime
 import base64
-import json
 
 CAPTURE_EXECUTABLE = 'ov2640_capture'
 
@@ -27,8 +26,8 @@ CameraCaptureResult = namedtuple(
     'filename'
 )
 
-class Camera:
 
+class Camera:
     def _image_to_string(self, filename=None):
         if filename is not None:
             try:
@@ -39,10 +38,10 @@ class Camera:
                 pass
         return ""
 
-    def capture(self, **kwargs):
-        filename = kwargs.get("filename", '{}.jpg'.format(datetime.now().strftime('%Y_%m_%d_%H_%M_%S')))
-        width = kwargs.get("width", X_RESOLUTION)
-        height = kwargs.get("height", Y_RESOLUTION)
+    def capture(self, filename=None, width=X_RESOLUTION, height=Y_RESOLUTION):
+
+        if not filename:
+            filename = '{}.jpg'.format(datetime.now().strftime('%Y_%m_%d_%H_%M_%S'))
 
         if len(filename) > 4 and filename[-4:] != '.jpg':
             filename += '.jpg'
@@ -61,7 +60,7 @@ class Camera:
         Telemetry.update("cam", packet)
         camera_result = CameraCaptureResult(filename)
         assert status == 0, "Failed to capture image with command {}".format(command)
-        return ""
+        return camera_result
 
     def capture_low(self, filename=None):
         return self.capture(filename=filename, width=resolutions[2][0], height=resolutions[2][1])
