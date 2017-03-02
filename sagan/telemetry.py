58714f8164aa26ae9e60edaddde1fa5d3c1cc757
AlexCollins <Alexander@MacBook-Pro.local>
2017-03-02 17:21:51 +1100

    environment variable for telemetry

--- sagan/telemetry.py
+++ sagan/telemetry.py
@@ -1,13 +1,19 @@
-_FIFO = open('/tmp/sagan_telemetry', 'w')
+import os
 import json
+
+_FIFO = None
+if os.environ['TELEMETRY'] == '1':
+    _FIFO = open('/tmp/sagan_telemetry', 'w')
+
 
 class Telemetry:
 
     @staticmethod
     def update(prefix: str, data: str):
         try:
-            _FIFO.write("{}:{}\n".format(prefix[0:3], json.dumps(data)))
-            _FIFO.flush()
+            if os.environ['TELEMETRY'] == '1':
+                _FIFO.write("{}:{}\n".format(prefix[0:3], json.dumps(data)))
+                _FIFO.flush()
         except:
             return False
         return True
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='sagan',
-    version='3.0.3',
+    version='3.0.4',
     packages=['sagan'],
     install_requires=["smbus-cffi", "RPIO"],
     url='',
