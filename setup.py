a9bc9c11e35afd7f7cac3fa4d901c6a952771921
AlexCollins <Alexander@vlan-2642-10-16-173-144.student.wireless.sydney.edu.au>
2017-03-06 12:58:23 +1100

    3.0.8

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='sagan',
-    version='3.0.7',
+    version='3.0.8',
     packages=['sagan'],
     install_requires=["smbus-cffi", "RPIO"],
     url='',
