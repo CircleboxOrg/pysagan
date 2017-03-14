150f1d65752872a16d4613d98c9b7bf9b82b5157
AlexCollins <Alexander@MacBook-Pro.local>
2017-03-14 12:11:46 +1100

    3.1.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='sagan',
-    version='3.1.0',
+    version='3.1.5',
     packages=['sagan'],
     install_requires=["smbus-cffi", "RPIO"],
     url='',
